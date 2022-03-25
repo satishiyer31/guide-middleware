14e2f5b0081ba3f6ba5998d0885d05f401bf4fde
satishiyer31 <satishiyer31@gmail.com>
2022-03-25 12:00:48 -0600

    Changed json file name

--- routes/oms.js
+++ routes/oms.js
@@ -3,7 +3,7 @@ const oms = require('express').Router();
 const {google}= require("googleapis");
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
+    keyFile: "google-credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   })
 
