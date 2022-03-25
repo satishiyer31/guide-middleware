4d2da5e49605ed48ab56b852e49a1ee1914e9e89
satishiyer31 <satishiyer31@gmail.com>
2022-03-25 14:38:07 -0600

    Filtering logic to extract records by email

--- routes/oms.js
+++ routes/oms.js
@@ -30,4 +30,33 @@ oms.get('/', async(req,res)=>{
 })
 
 
+oms.get('/:email', async(req,res)=>{
+
+    //create client instance for auth
+   const client = await auth.getClient();
+ 
+   //Instance of API
+   const googleSheets = google.sheets({version: "v4", auth: client });
+     
+   const getRows = await googleSheets.spreadsheets.values.get({
+     
+       auth,
+       spreadsheetId,
+       range: "Sheet1",
+     });
+    //  console.log(getRows.data.values[1][1])
+    //  res.json(getRows.data.values);
+     const rows = getRows.data.values;
+     const filteredRows = []
+
+     for (const row of rows) {
+         if ((row[0]) == req.params.email) {
+             filteredRows.push(row)
+         }
+     }
+
+     res.json(filteredRows);
+
+ })
+
 module.exports = oms;
