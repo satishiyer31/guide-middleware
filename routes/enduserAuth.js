14f2988a4157513cabca6b40ba1b4a10e9638ad9
satishiyer31 <satishiyer31@gmail.com>
2023-11-01 21:20:46 -0600

    jwt post changes

--- routes/enduserAuth.js
+++ routes/enduserAuth.js
@@ -1,13 +1,18 @@
 const enduserAuth = require('express').Router();
-const signToken = require('../helpers/fsUtils');
+//const signToken = require('../helpers/fsUtils');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env;
 
-
-enduserAuth.post('/jwt', async (req,res) => {
+enduserAuth.post('/', async (req,res) => {
 
 if (req.body) {
-    const token = await signToken(req.body);
+
+    const payload = req.body
+    console.log(payload);
+    const expiration = '2h'
+    const token = jwt.sign({ data: payload }, JWT_SECRET, { expiresIn: expiration });
     console.log(token)
-    res.status(201).json(token) ;
+    res.status(201).json(token);
 }
 
     
