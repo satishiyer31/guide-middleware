4bbb8c93dea2fb3f593ea155ad469ea14cbb449e
satishiyer31 <satishiyer31@gmail.com>
2023-11-02 12:16:07 -0600

    test with hard creds

--- routes/enduserAuth.js
+++ routes/enduserAuth.js
@@ -11,7 +11,13 @@ if (req.body) {
     const KEY_ID = 'app_6542ebfb3a057401be95fe79'
     // console.log(payload);
     // const expiration = '2h'
-    const token = jwt.sign({payload }, JWT_SECRET, {expiresIn: '1h', header:{kid:KEY_ID}});
+    const token = jwt.sign({
+        "email": "eric.s.smith411@gmail.com",
+        "scopr": "user",
+        "external_id": "0034x00000OdUn0AAF",
+        "name": "Erica Lee"
+    
+    }, JWT_SECRET, {expiresIn: '1h', header:{kid:KEY_ID}});
     // console.log(token)
     res.status(201).json(token);
 }
