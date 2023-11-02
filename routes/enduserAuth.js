918bd0894cce77f8efcadf05fad22df0b6a5c5c2
satishiyer31 <satishiyer31@gmail.com>
2023-11-02 12:38:20 -0600

    corrected extra object from the jwt signin

--- routes/enduserAuth.js
+++ routes/enduserAuth.js
@@ -7,11 +7,12 @@ enduserAuth.post('/', async (req,res) => {
 
 if (req.body) {
 
-    const payload = JSON.parse(req.body)
+    const payload = req.body
+    console.log(payload)
     const KEY_ID = 'app_6542ebfb3a057401be95fe79'
     // console.log(payload);
     // const expiration = '2h'
-    const token = jwt.sign({payload}, JWT_SECRET, {expiresIn: '1h', header:{kid:KEY_ID}});
+    const token = jwt.sign(payload, JWT_SECRET, {expiresIn: '1h', header:{kid:KEY_ID}});
     // console.log(token)
     res.status(201).json(token);
 }
