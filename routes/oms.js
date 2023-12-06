3adbbd0adfb701995f5b88ba17e34ef3343d9110
satishiyer31 <satishiyer31@gmail.com>
2023-12-05 22:52:27 -0700

    Adding secure integration route to OMS

--- routes/oms.js
+++ routes/oms.js
@@ -13,24 +13,78 @@ const auth = new google.auth.GoogleAuth({
 
 const spreadsheetId ="1ZWTkdTq0PMnJzWziSvRf-EGHnd6_HO8on7R8aRXNrlE";
 
-// oms.get('/', async(req,res)=>{
+oms.get('/', async(req,res)=>{
 
-//    //create client instance for auth
-//   const client = await auth.getClient();
+    try {
 
-//   //Instance of API
-//   const googleSheets = google.sheets({version: "v4", auth: client });
-    
-//   const getRows = await googleSheets.spreadsheets.values.get({
-    
-//       auth,
-//       spreadsheetId,
-//       range: "Sheet1",
-//     });
-//     console.log(getRows.data.values[1][1])
-//     res.json(getRows.data.values);
-// })
+            const data = await ProcessToken(req,res)
+        // console.log(data)
+        if(!data) {
+            throw Error('Invalid Authorization')
+        }
+        const email = data.email
 
+        //create client instance for auth
+        const client = await auth.getClient();
+        
+        //Instance of API
+        const googleSheets = google.sheets({version: "v4", auth: client });
+            
+        const getRows = await googleSheets.spreadsheets.values.get({
+            
+            auth,
+            spreadsheetId,
+            range: "Sheet1",
+            });
+            //  console.log(getRows.data.values[1][1])
+            //  res.json(getRows.data.values);
+            const rows = getRows.data.values;
+            const filteredRows = []
+
+            for (const row of rows) {
+                if ((row[0]) == email) {
+                    filteredRows.push(row)
+                }
+            }
+
+            res.json(filteredRows);
+    }
+    catch (error){
+
+    }
+ })
+
+ async function ProcessToken(request,response) {
+
+    try {
+      const token = await getTokenfromHeader(request)
+      const validatedPayload = await validateToken(token)
+      // console.log(validatedPayload)
+      return validatedPayload
+    }
+    catch (err){
+      response.status(401).json({message:"Authentication Error"})
+    }
+
+  }
+
+  async function validateToken(token){
+
+    const decodedToken = jwt.decode(token, {complete: true})
+    const kid = decodedToken.header.kid
+    const signingKey = await jwksClientInstance.getSigningKey(kid)
+    return jwt.verify(token,signingKey.rsaPublicKey)
+  }
+
+  async function getTokenfromHeader(request) {
+
+    const auth = request.headers.authorization
+  if (!auth) {
+    throw Error('Auth Header missing')
+  }
+  const token = auth.split(" ")[1]
+  return token
+  }
 
 oms.get('/:email', async(req,res)=>{
 
